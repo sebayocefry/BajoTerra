248d1f86b83f1bb4cf446da43f070dd9dae98697
gabriel becerra <gabriel.bc2005@gmail.com>
2026-04-05 02:41:29 -0400

    cree 2 carpetas  no esta ordenada como corresponden
    cree 2 carpetas "nivelPrueva" y "extra_brr" quería ver y  probar los personaje y el sistema de disparo pero como sabia como usar los suyos  hice el mio para probar y saber como funciona el godot pero cada carpeta y código se puede mover donde corresponde para tener el orden que acordamos

---
+++ escenas/niveles/extra_brr/bala.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var pos:Vector2
+
+var rota:float
+
+var dir : float
+
+var speed= 2000
+
+
+func _ready():
+	global_position=pos
+	global_rotation=rota
+	
+func _physics_process(delta):
+	velocity=Vector2(speed,0).rotated(dir)
+	move_and_slide()
