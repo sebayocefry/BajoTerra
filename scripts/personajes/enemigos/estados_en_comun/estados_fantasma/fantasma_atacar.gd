fb31555a97d2368f8c34ee69ac51ae1f21d4e18a
Luis Juarez Torres Guerrero <luis.juarez@alumnos.ucn.cl>
2026-06-26 15:19:36 -0400

    restaurar disparo de bolas de fuego en Chonchon y Fantasma tras refactor de estados

--- scripts/personajes/enemigos/estados_en_comun/estados_fantasma/fantasma_atacar.gd
+++ scripts/personajes/enemigos/estados_en_comun/estados_fantasma/fantasma_atacar.gd
@@ -1,1 +1,15 @@
 extends "res://scripts/personajes/enemigos/estados_en_comun/atacar.gd"
+
+func ejecutar_golpe():
+	if enemigo.has_method("actualizar_animacion"):
+		enemigo.actualizar_animacion("attack")
+
+	# Telegraphing: pequeña espera antes de soltar el disparo
+	await get_tree().create_timer(0.4).timeout
+
+	if not is_instance_valid(enemigo) or not is_instance_valid(enemigo.jugador):
+		return
+
+	var distancia = enemigo.global_position.distance_to(enemigo.jugador.global_position)
+	if enemigo.jugador_en_rango or distancia <= enemigo.distancia_ataque:
+		enemigo.disparar()
